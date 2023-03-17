e7838b7685125f3ca28ad41244607afdff115a6b
Akash Kumar Singh <itsakash18.06@gmail.com>
2023-03-17 16:20:23 +0530

    calc

--- calc.js
+++ calc.js
@@ -2,10 +2,14 @@ function clearScreen() {
     document.getElementById("result").value = ""
 }
 
+function cls() {
+    let current = document.getElementById("result").value
+    document.getElementById("result").value = current.slice(0, -1)
+}
+
 function display(value) {
     document.getElementById("result").value += value
 }
-
 
 function calculate1() {
     let v = document.getElementById("result").value
